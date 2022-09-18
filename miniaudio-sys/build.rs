f7f3b8dd3d2efc3220e5def6587b98e3922fb221
krazijames <krazijames@gmail.com>
2022-09-18 23:36:22 +0900

    Build binding for each platform to prevent memory crach

--- miniaudio-sys/build.rs
+++ miniaudio-sys/build.rs
@@ -10,23 +10,18 @@ fn main() {
         .flag_if_supported("-Wno-unused-function")
         .compile("miniaudio");
 
-    #[cfg(feature = "bindgen")]
-    generate_bindings();
-}
-
-#[cfg(feature = "bindgen")]
-fn generate_bindings() {
     bindgen::Builder::default()
         .header("miniaudio/extras/miniaudio_split/miniaudio.h")
-        .raw_line("#![allow(non_upper_case_globals)]")
-        .raw_line("#![allow(non_camel_case_types)]")
-        .raw_line("#![allow(non_snake_case)]")
         .impl_debug(true)
         .prepend_enum_name(false)
         .rustfmt_bindings(true)
         .size_t_is_usize(true)
         .generate()
         .expect("unable to generate bindings")
-        .write_to_file("src/lib.rs")
+        .write_to_file("src/bindings.rs")
         .expect("couldn't write bindings");
+
+    println!("cargo:rerun-if-changed=miniaudio/extras/miniaudio_split/miniaudio.h");
+    println!("cargo:rerun-if-changed=miniaudio/extras/miniaudio_split/miniaudio.c");
+    println!("cargo:rerun-if-env-changed=CC");
 }
