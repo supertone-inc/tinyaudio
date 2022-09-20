404734e0a3ac4cace000bfd098ba2581531937c7
krazijames <krazijames@gmail.com>
2022-09-21 03:06:31 +0900

    Configure debugger

--- tinyaudio-cpp/test.sh
+++ tinyaudio-cpp/test.sh
@@ -7,4 +7,7 @@ BUILD_DIR=build
 cmake -S. -B$BUILD_DIR -DCMAKE_BUILD_TYPE=Debug -DTINYAUDIO_BUILD_TESTS=ON
 cmake --build $BUILD_DIR --config Debug
 cmake --install $BUILD_DIR --config Debug --prefix $BUILD_DIR
-./$BUILD_DIR/bin/tinyaudio-test
+
+if [[ -z $NO_RUN ]]; then
+    ./$BUILD_DIR/bin/tinyaudio-test
+fi
