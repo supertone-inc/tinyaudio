b4da8bcea125b01a5c405828b5d8c62afdc9b739
krazijames <krazijames@gmail.com>
2022-09-21 01:53:45 +0900

    Run doctest test binary instead of running ctest

--- tinyaudio-cpp/test.sh
+++ tinyaudio-cpp/test.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-BUILD_DIR=./build
+BUILD_DIR=build
 
-cmake -S. -B$BUILD_DIR -DTINYAUDIO_BUILD_TESTS=ON
-cmake --build $BUILD_DIR
-ctest --test-dir $BUILD_DIR -V --no-tests=error
+cmake -S. -B$BUILD_DIR -DCMAKE_BUILD_TYPE=Debug -DTINYAUDIO_BUILD_TESTS=ON
+cmake --build $BUILD_DIR --config Debug
+cmake --install $BUILD_DIR --config Debug --prefix $BUILD_DIR
+./$BUILD_DIR/bin/tinyaudio-test
