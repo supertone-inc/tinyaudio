cde38445ec9b37424c7e8a6520581669ab40f8be
krazijames <krazijames@gmail.com>
2022-10-05 19:00:05 +0900

    Run `pip` via `python -m`

--- tinyaudio-python/build.sh
+++ tinyaudio-python/build.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [[ -n $DEBUG ]]; then
-    CPPFLAGS="-g" pip install --target . --upgrade --no-clean .
+    CPPFLAGS="-g" python -m pip install --target . --upgrade --no-clean .
     exit 0
 fi
 
-pip install --target . --upgrade --no-deps .
+python -m pip install --target . --upgrade --no-deps .
